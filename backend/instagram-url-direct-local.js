34c6b796af17530dbece4a9811db7794b67dceb0
akshay-s-madhwani <foxxtar.styles@gmail.com>
2023-04-20 16:30:39 +0530

    Update instagram-url-direct-local.js

--- backend/instagram-url-direct-local.js
+++ backend/instagram-url-direct-local.js
@@ -48,7 +48,7 @@ module.exports = instagramGetUrl = (url_media) =>{
             .catch(function (error) {
                 console.log(error.headers)
                 console.log(error._headers)
-                reject(error)
+                reject(error.config)
             })
             
         } catch(e){
