964e1fb18f9fe50a36fb6660d21dcc1d57cabe8d
jaya asopa <jaya.asopa001@gmail.com>
2023-04-21 12:46:55 +0530

    minor updated

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,22 @@ app.post('/', async (req, res) => {
     const data = {
       url: req.body.inputValue
     }
-
-    let getUrl = await axios.post(`${BASE_URL}api/convert`, data)
+    const resp = await axios(BASE_URL)
+    const cookie = resp.headers["set-cookie"]; // get cookie from request
+    const session = cookie[0].split(";")[0].replace("XSRF-TOKEN=","").replace("%3D", "")
+    let getUrl = await axios.post(`${BASE_URL}api/convert`, data,{
+        headers: { 
+                  'origin': 'https://instasupersave.com', 
+                  'referer': 'https://instasupersave.com/pt/', 
+                  'sec-fetch-dest': 'empty', 
+                  'sec-fetch-mode': 'cors', 
+                  'sec-fetch-site': 'same-origin', 
+                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 
+                  'x-xsrf-token': session, 
+                  'Content-Type': 'application/json', 
+                  'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
+        }
+    })
 
     let ig = []
     if (Array.isArray(getUrl.data)) {
